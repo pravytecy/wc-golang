bfa4ea3bfd69dbf13691539e930218f354b6ac8c
pravytecy <pravyedtech@gmail.com>
2023-05-24 08:18:40 +0530

    positional args support

--- main.go
+++ main.go
@@ -6,28 +6,31 @@ package main
 import (
 	"bufio"
 	"errors"
+	"flag"
 	"fmt"
 	"io"
 	"os"
-	"strconv"
 )
 
 type config struct {
 	numTimes   int
 	printUsage bool
+	name       string
 }
 
 var usageString = fmt.Sprintf(`Usage: %s  [-h|--help] A greeter application which prints the name you entered  number of times.`, os.Args[0])
+var errPosArgSpecified = errors.New("positional args specifed")
 
 func printUsage(w io.Writer) {
 	fmt.Fprintf(w, "%s", usageString)
 }
 
 func main() {
-	c, err := parseArgs(os.Args[1:])
+	c, err := parseArgs(os.Stderr, os.Args[1:])
 	if err != nil {
-		fmt.Fprintln(os.Stdout, err)
-		printUsage(os.Stdout)
+		if errors.Is(err, errPosArgSpecified) {
+			fmt.Fprintln(os.Stdout, err)
+		}
 		os.Exit(1)
 	}
 	err = validateArgs(c)
@@ -50,29 +53,58 @@ func validateArgs(c config) error {
 	return nil
 }
 
-func parseArgs(args []string) (config, error) {
-	var numTimes int
-	var err error
-
+func parseArgs(w io.Writer, args []string) (config, error) {
 	c := config{}
-	if len(args) != 1 {
-		return c, errors.New("invalid number of arguments")
+	fs := flag.NewFlagSet("Jerry printer", flag.ContinueOnError)
+	fs.SetOutput(w)
+
+	fs.Usage = func() {
+		var usageString = `A greeter application which prints the name you entered a specified number of times. Usage of %s:  [name]`
+		fmt.Fprintf(w, usageString, fs.Name())
+		fmt.Fprintln(w)
+
+		fmt.Fprintln(w, "Options: ")
+		fs.PrintDefaults()
+
 	}
-	if args[0] == "-h" || args[0] == "--help" {
-		c.printUsage = true
-		return c, nil
-	}
-	if args[0] == "-l" {
-		c.printUsage = true
-		return c, nil
-	}
-	numTimes, err = strconv.Atoi(args[0])
+	fs.IntVar(&c.numTimes, "n", 1, "Number of times to greet")
+	fs.StringVar(&c.name, "a", "", "Welcome")
+	err := fs.Parse(args)
 	if err != nil {
 		return c, err
 	}
-	c.numTimes = numTimes
+	if fs.NArg() > 1 {
+		return c, errPosArgSpecified
+	}
+	if fs.NArg() == 1 {
+		c.name = fs.Arg(0)
+	}
 	return c, nil
 }
+
+// func parseArgs(args []string) (config, error) {
+// 	var numTimes int
+// 	var err error
+
+// 	c := config{}
+// 	if len(args) != 1 {
+// 		return c, errors.New("invalid number of arguments")
+// 	}
+// 	if args[0] == "-h" || args[0] == "--help" {
+// 		c.printUsage = true
+// 		return c, nil
+// 	}
+// 	if args[0] == "-l" {
+// 		c.printUsage = true
+// 		return c, nil
+// 	}
+// 	numTimes, err = strconv.Atoi(args[0])
+// 	if err != nil {
+// 		return c, err
+// 	}
+// 	c.numTimes = numTimes
+// 	return c, nil
+// }
 
 func getName(r io.Reader, w io.Writer) (string, error) {
 	msg := "Your name please? Press the Enter key when done.\n"
@@ -90,21 +122,20 @@ func getName(r io.Reader, w io.Writer) (string, error) {
 }
 
 func runCmd(r io.Reader, w io.Writer, c config) error {
-	if c.printUsage {
-		printUsage(w)
-		return nil
+	var err error
+	if len(c.name) == 0 {
+		c.name, err = getName(r, w)
+		if err != nil {
+			return err
+		}
 	}
-	name, err := getName(r, w)
-	if err != nil {
-		return err
-	}
-	greetUser(c, name, w)
+	greetUser(c, w)
 	return nil
 }
 
-func greetUser(c config, name string, w io.Writer) {
-	msg := fmt.Sprintf("Nice to meet you %s\n", name)
+func greetUser(c config, w io.Writer) {
+	msg := fmt.Sprintf("Nice to meet you %s\n", c.name)
 	for i := 0; i < c.numTimes; i++ {
-		fmt.Fprintf(w, "%s", msg)
+		fmt.Fprintf(w, msg)
 	}
 }
